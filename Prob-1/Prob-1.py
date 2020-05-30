bba2bbf42032d39f1e3507231473a92a5a1be304
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-30 12:50:28 -0700

    i found a youtube video that helped me understand

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,9 +2,27 @@
 #   Programming Assignment 10
 #     Prob-1.py
 
-# <YOUR NAME>
+# Eddy
+# Write a program that asks the user for numbers, and provides the sum of the numbers when finished.
+
+# Valid numbers are non-negative. The user signals the end of the list by entering a negative number.
+# not for its while loop
+# if number is less then 0 exit the loop
+
 
 def main():
-    # your code here
+    total = 0
+    usernumber = float(input("lets start by entering a number: "))
 
-main()    
+    while True:
+        if usernumber >= 0:
+            total = total + usernumber
+            usernumber = float(
+                input("now enter another number, or enter a negitve number to stop: "))
+        else:
+            break
+
+    print("the grand total is: ", total)
+
+
+main()
